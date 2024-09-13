4d0377afd73c5848790eb1d531319d788c20baa8
Ziero25 <guiluccamoraes@hotmail.com>
2024-09-12 21:17:35 -0300

    fix bug Marcadas/Desmarcadas

--- index.js
+++ index.js
@@ -61,14 +61,14 @@ const listarMetas = async () => {
         choices: [...metas],
         instructions: false
     })
+    metas.forEach((m) =>{
+        m.checked = false
+    })
+
     if(respostas.length ==0 ){
         console.log("Nenhuma meta selecionada!")
         return listarMetas()
     }
-
-    metas.forEach((m) =>{
-        m.checked = false
-    })
 
     respostas.forEach((resposta) => {
         const meta = metas.find((m) => {
